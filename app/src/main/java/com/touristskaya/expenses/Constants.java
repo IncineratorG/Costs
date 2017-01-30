a4ee8ac256a48d32c2dd3257413a6cd8cde97868
Alexander <alexander.v.dorohov@yandex.ru>
2017-01-30 15:03:58 +0300

    Input data bug fixed

--- app/src/main/java/com/touristskaya/expenses/Constants.java
+++ app/src/main/java/com/touristskaya/expenses/Constants.java
@@ -24,15 +24,7 @@ public class Constants {
 //            "Двадцать девятый", "Тридцатый", "Тридцать первый"};
     public static final String[] DAY_NAMES = {"", "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"};
 
-    public static final String SEPARATOR_VALUE = "$";
-    public static final String SEPARATOR_DATE = "#";
-    public static final String SEPARATOR_ID = "&";
-    public static final String SEPARATOR_MILLISECONDS = "%";
-    public static final String SEPARATOR_NOTE = "~";
 
-    public static final int HEADER_SYSTEM_COLOR = Color.parseColor("#FF9800");
-    public static final String tag = "tag";
-    public static final String DATA_ARRAY_LABEL = "dataArray";
     public static final String EXPENSE_DATA_UNIT_LABEL = "expense_data_unit";
     public static final String EDIT_DIALOG_TAG = "edit_dialog_tag";
     public static final String CHOOSE_STATISTIC_PERIOD_DIALOG_TAG = "choose_period_dialog_tag";
@@ -47,22 +39,8 @@ public class Constants {
     public static final int STATISTIC_DETAILED_ACTIVITY = 102;
     public static final int FRAGMENT_STATISTIC_MAIN_SCREEN = 103;
     public static final int EDIT_DATA_ACTIVITY = 104;
-    public static final int STATISTIC_COST_TYPE_DETAILED_ACTIVITY = 105;
-    public static final int STATISTIC_CHOSE_PERIOD_ACTIVITY = 106;
-    public static final int STATISTIC_CHOSEN_PERIOD_ACTIVITY = 107;
 
     public static final String SAVED_VALUE = "savedvalue";
-    public static final int INITIAL_DATE_IN_MILLISECONDS_INDEX = 0;
-    public static final int ENDING_DATE_IN_MILLISECONDS_INDEX = 1;
-    public static final int INITIAL_DATE_STRING_INDEX = 2;
-    public static final int ENDING_DATE_STRING_INDEX = 3;
-    public static final int COST_NAME_INDEX = 4;
-    public static final int COST_VALUE_INDEX = 5;
-    public static final int CHOSEN_MONTH_INDEX = 6;
-    public static final int CHOSEN_YEAR_INDEX = 7;
-    public static final int COST_ID_INDEX = 8;
-    public static final int COST_NOTE_INDEX = 9;
-    public static final int DATA_ARRAY_SIZE = 10;
 
     public static final int EDIT_EXPENSE_RECORD_DIALOG_REQUEST_CODE = 777;
     public static final int DELETE_ITEM = 11;
